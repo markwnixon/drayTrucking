b2bb229b8340fee70f957852d442f0ccc41769b0
Mark Nixon <mark@nixonai.com>
2025-02-04 17:02:56 -0500

    Initial Commit

---
+++ drayTrucking/drayTruckingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drayTruckingApp.swift
+//  drayTrucking
+//
+//  Created by Mark Nixon on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct drayTruckingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
